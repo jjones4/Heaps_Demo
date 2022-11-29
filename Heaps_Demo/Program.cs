4d0918a887977d89faf8b574a11fb2f28ba2a4e6
Alma <alma@alma-c-box.local>
2022-11-28 19:43:39 -0600

    Add Code to Create an Array with Random Data to Heapify

--- Heaps_Demo/Program.cs
+++ Heaps_Demo/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine();
+
+            // STEP 1
+
+            /*
+             * 
+             * Set up some random data in an array.
+             * 
+             * Make the array a random length from 0 to 19,
+             * and fill it with random numbers from 0 to 19.
+             * 
+            */
+            Random rnd = new Random();
+
+            int numberOfNodes = rnd.Next(20);
+
+            Console.WriteLine($"   The length of our data is: {numberOfNodes}\n");
+
+            Console.Write("   Our initial data array is: [ ");
+
+            int[] data = new int[numberOfNodes];
+
+            for (int i = 0; i < numberOfNodes; i++)
+            {
+                data[i] = rnd.Next(20);
+
+                Console.Write($"{data[i]} ");
+            }
+
+            Console.Write("]");
+
+            Console.WriteLine();
+
+            // STEP 2
+
+            /*
+             * 
+             * Create our binary tree from the array data.
+             * 
+            */
         }
     }
 }
